d4ee9cfc307970cb757d9109a47ba10b4c8e8c3c
Cristian Prieto <me@cprieto.com>
2015-07-14 19:16:00 +1000

    Initial version for our provider

---
+++ configurator.autofac/IConfigurationSourceProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace configurator.autofac
+{
+    public interface IConfigurationSourceProvider
+    {
+        IDictionary<string, string> GetValues();
+    }
+}
