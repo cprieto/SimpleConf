e0f735c7ca9a06a43ba927fe451d491a8c7611fb
Cristian Prieto <me@cprieto.com>
2015-07-14 19:46:24 +1000

    Adding test for environment provider

---
+++ configurator.autofac.tests/EnvironmentConfigurationSourceProviderFacts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace configurator.autofac.tests
+{
+    public class EnvironmentConfigurationSourceProviderFacts : IDisposable
+    {
+        private readonly string _value;
+        private readonly string _key;
+        private readonly EnvironmentConfigurationSourceProvider _provider = new EnvironmentConfigurationSourceProvider();
+
+        public EnvironmentConfigurationSourceProviderFacts()
+        {
+            _key = Guid.NewGuid().ToString("N");
+            _value = new Random().Next(10).ToString();
+
+            Environment.SetEnvironmentVariable(_key, _value);
+        }
+
+        [Fact]
+        public void ItGetsEnvironmentVariableFromDictionary()
+        {
+            var values = _provider.GetValues();
+
+            Assert.NotNull(values[_key]);
+            Assert.Equal(_value, values[_key]);
+        }
+
+        [Fact]
+        public void IfValueDoesntExistItIsNotPresent()
+        {
+            var values = _provider.GetValues();
+            var notFound = Guid.NewGuid().ToString("N");
+
+            Assert.Null(values[notFound]);
+        }
+
+        public void Dispose()
+        {
+            Environment.SetEnvironmentVariable(_key, null);
+        }
+    }
+}
---
+++ configurator.autofac/EnvironmentConfigurationSourceProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace configurator.autofac
+{
+    public class EnvironmentConfigurationSourceProvider : IConfigurationSourceProvider
+    {
+        private readonly Lazy<IDictionary> _environment = 
+            new Lazy<IDictionary>(Environment.GetEnvironmentVariables);
+
+        public IDictionary<string, string> GetValues()
+        {
+            var dict = _environment.Value;
+            return dict.Keys.Cast<string>()
+                .ToDictionary(key => key, key => dict[key] as string);
+        }
+    }
+}
