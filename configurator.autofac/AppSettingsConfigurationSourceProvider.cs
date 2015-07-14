1a24123b15bea35723b4b4680057038bdbd430e3
Cristian Prieto <me@cprieto.com>
2015-07-14 19:58:26 +1000

    Nuget exe is needed

---
+++ configurator.autofac/AppSettingsConfigurationSourceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace configurator.autofac
+{
+    public class AppSettingsConfigurationSourceProvider : IConfigurationSourceProvider
+    {
+        public IDictionary<string, string> GetValues()
+        {
+            var settings = ConfigurationManager.AppSettings;
+            return settings.Keys.Cast<string>()
+                .ToDictionary(key => key, key => settings[key]);
+        }
+    }
+}
