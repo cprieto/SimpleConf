591b333da2d1005bfb64c42a2f7797def40dee51
Cristian Prieto <me@cprieto.com>
2015-07-15 19:15:14 +1000

    Added configuration builder

---
+++ configurator.autofac/ConfiguratorBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace configurator.autofac
+{
+    public sealed class ConfiguratorBuilder
+    {
+        private readonly IList<IDictionary<string, string>> _sources = new List<IDictionary<string, string>>();
+
+        public ConfiguratorBuilder FromEnvironment()
+        {
+            return FromSource(new EnvironmentConfigurationSourceProvider());
+        }
+
+        public ConfiguratorBuilder FromAppSettings()
+        {
+            return FromSource(new AppSettingsConfigurationSourceProvider());
+        }
+
+        public ConfiguratorBuilder FromSource<TSource>() where TSource : IConfigurationSourceProvider, new()
+        {
+            var source = Activator.CreateInstance<TSource>();
+            _sources.Add(source.GetValues());
+
+            return this;
+        }
+
+        public ConfiguratorBuilder FromSource<TSource>(TSource source) where TSource : IConfigurationSourceProvider
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+
+            _sources.Add(source.GetValues());
+
+            return this;
+        }
+
+        public IList<IDictionary<string, string>> Build()
+        {
+            return _sources;
+        }
+    }
+}
