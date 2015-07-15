de02c0a4c6d511ee5d734fcc919c86fa009cc0f6
Cristian Prieto <me@cprieto.com>
2015-07-15 19:01:59 +1000

    Added adapter for multiple dictionaries

---
+++ configurator.autofac.tests/CascadingMultipleDictionaryAdapterFacts.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace configurator.autofac.tests
+{
+    public class CascadingMultipleDictionaryAdapterFacts
+    {
+        [Fact]
+        public void WithNoInputItJustThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CascadingMultipleDictionaryAdapter(null));
+        }
+
+        [Fact]
+        public void EmptyInputIsOk()
+        {
+            // ReSharper disable once UnusedVariable
+            var dummy = new CascadingMultipleDictionaryAdapter(new List<IDictionary<string, string>>());
+        }
+
+        [Fact]
+        public void DefaultCtorIsOk()
+        {
+            var adapter = new CascadingMultipleDictionaryAdapter();
+
+            Assert.NotNull(adapter.Sources);
+            Assert.Empty(adapter.Sources);
+        }
+
+        [Fact]
+        public void AddingNullSourceThrows()
+        {
+            var adapter = new CascadingMultipleDictionaryAdapter();
+
+            Assert.Throws<ArgumentNullException>(() => adapter.AddSource(null));
+        }
+
+        [Fact]
+        public void AddingEmptySourceIsOk()
+        {
+            var adapter = new CascadingMultipleDictionaryAdapter();
+            adapter.AddSource(new Dictionary<string, string>());
+
+            Assert.NotEmpty(adapter.Sources);
+        }
+
+        [Fact]
+        public void ItContainsTheKeysOfSourceDictionaries()
+        {
+            var dict1 = new Dictionary<string, string> { { "item1", "value1" } };
+            var dict2 = new Dictionary<string, string> { { "item2", "value2" } };
+
+            var adapter = new CascadingMultipleDictionaryAdapter(new[] { dict1, dict2 });
+
+            Assert.Equal(2, adapter.Sources.Count());
+            Assert.True(adapter.Contains("item1"));
+            Assert.True(adapter.Contains("item2"));
+            Assert.False(adapter.Contains("item3"));
+        }
+
+        [Fact]
+        public void ItGetsTheValueFomDictionary()
+        {
+            var dict1 = new Dictionary<string, string> { { "item1", "value1" } };
+            var dict2 = new Dictionary<string, string> { { "item2", "value2" } };
+
+            var adapter = new CascadingMultipleDictionaryAdapter(new[] { dict1, dict2 });
+
+            Assert.Equal("value1", adapter["item1"]);
+            Assert.Equal("value2", adapter["item2"]);
+            Assert.Null(adapter["item3"]);
+        }
+
+        [Fact]
+        public void OrderAffectValuesWhenTheSameKeyIsPresent()
+        {
+            var dict1 = new Dictionary<string, string> { { "item1", "value1" } };
+            var dict2 = new Dictionary<string, string> { { "item1", "value2" } };
+
+            var adapter = new CascadingMultipleDictionaryAdapter(new[] { dict1, dict2 });
+
+            Assert.Equal("value2", adapter["item1"]);
+        }
+    }
+}
---
+++ configurator.autofac/CascadingMultipleDictionaryAdapter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.Components.DictionaryAdapter;
+
+namespace configurator.autofac
+{
+    public class CascadingMultipleDictionaryAdapter : AbstractDictionaryAdapter
+    {
+        private readonly IList<IDictionary<string, string>> _sources = new List<IDictionary<string, string>>();
+
+        public CascadingMultipleDictionaryAdapter()
+        {
+        }
+
+        public CascadingMultipleDictionaryAdapter(IEnumerable<IDictionary<string, string>> sources)
+        {
+            if (sources == null)
+                throw new ArgumentNullException();
+
+            _sources = new List<IDictionary<string, string>>(sources);
+        }
+
+        public IEnumerable<IDictionary<string, string>> Sources
+        {
+            get { return _sources; }
+        }
+
+        public void AddSource(IDictionary<string, string> source)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+
+            _sources.Add(source);
+        }
+
+        public override object this[object key]
+        {
+            get
+            {
+                string value;
+                return TryGet(key as string, out value) ? value : null;
+            }
+            set { throw new NotSupportedException(); }
+        }
+
+        public override bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public override bool Contains(object key)
+        {
+            if (key == null)
+                throw new ArgumentNullException();
+
+            return _sources.Any(x => x.ContainsKey(key as string));
+        }
+
+        public bool TryGet(string key, out string value)
+        {
+            foreach (var source in Sources.Reverse())
+            {
+                if (source.TryGetValue(key, out value))
+                    return true;
+            }
+            value = null;
+            return false;
+        }
+    }
+}
