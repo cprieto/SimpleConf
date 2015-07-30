9a97fdc3758eb63700a2fa86d574a42fd124994a
Cristian Prieto <me@cprieto.com>
2015-07-30 11:49:57 -0400

    Uploading results for coverall from build

--- runtests.ps1
+++ runtests.ps1
@@ -8,3 +8,14 @@ $target = "-target:$xunit"
 $targetargs = "-targetargs:$tests -noshadow"
 $filter = "-filter:+[SimpleConf*]* -[SimpleConf.Tests*]*"
 &$opencover $target $targetargs -register:user $filter -output:coverage.xml
+
+If (Test-Path env:APPVEYOR) {
+    $coverage_result = Get-ChildItem -Filter coverage.xml -Recurse | Resolve-Path -Relative
+    $coverall_args = "--opencover $coverage_result -r $env:COVERALL_TOKEN"
+    $coverall = (Get-ChildItem -Filter "coverall.net.exe" -Recurse | Resolve-Path -Relative).FullName
+    
+    &$coverall $coverall_args 
+} else {
+    Write-Host -ForegroundColor Green "Not running in CI"
+}
+
